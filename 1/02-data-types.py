d50d80d0b68dff302a2acd101995a9b09a36c23b
Halyna Urbanska <aquilla@ukr.net>
2024-04-16 09:05:15 +0300

    L-1::Lesson 1

---
+++ 1/02-data-types.py
@@ -0,0 +1,20 @@
+x = 100
+y = 3.14
+str = "Hello"
+isTrue = True
+none = None
+
+print(type(x))
+print(type(y))
+print(type(str))
+print(type(isTrue))
+print(none)
+print(type(none))
+
+a = 0
+print(a)
+print(type(a))
+
+none = 10
+print(none)
+print(type(none))
---
+++ 1/04-logical-operators.py
@@ -0,0 +1,11 @@
+a = 3
+b = 2
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a >= b and a > b)
+print(a == b or a > b)
+print(not a + b == b + a)
